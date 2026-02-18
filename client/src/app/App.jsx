4c49805539bd4733bd8fe6dbbe4f608c3baeea22
Miss-Melissa <melissahh@live.se>
2026-02-18 20:37:46 +0100

    center tasks page and dashboard page

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,27 +1,28 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "../components/Navbar/Navbar";
 
 
-import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
-import Tasks from "./pages/Tasks/Tasks";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import Task from "./pages/Task/Task";
-import Profile from "./pages/Profile/Profile";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import Tasks from "../pages/Tasks/Tasks";
+import Dashboard from "../pages/Dashboard/Dashboard";
+import Task from "../pages/Task/Task";
+import Profile from "../pages/Profile/Profile";
 
-import PrivateRoute from "./routes/PrivateRoute";
+import PrivateRoute from "../routes/PrivateRoute";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Footer from "./components/Footer/Footer";
+import Footer from "../components/Footer/Footer";
+import "./App.scss";
 
 function App() {
   return (
     <div className="app">
 
       {/* ✅ ONE GLOBAL CONTAINER */}
-      < ToastContainer
+      <ToastContainer
         position="top-right"
         autoClose={3000}
         pauseOnHover
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import App from "./App";
 import AuthProvider from "./context/AuthContext/AuthProvider";
 import ThemeProvider from "./context/ThemeContext/ThemeProvider";
 
 import "./styles/main.scss";
+import App from "./app/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
